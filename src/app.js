320057c6b030d1b5addadd7d3d60e8bf6f405b5f
Admin-MS-hub <administrador@massalud.org.pe>
2024-11-25 15:20:19 -0500

    localhost

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 dotenv.config();
 const corsOptions = {
-    origin: ['https://darkgrey-ostrich-727682.hostingersite.com'], // Orígenes permitidos
+    origin: ['https://darkgrey-ostrich-727682.hostingersite.com', 'http://localhost:3000'], // Orígenes permitidos
     methods: ['OPTIONS', 'GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
     credentials: true, // Permite enviar cookies y encabezados de autorización
 };
