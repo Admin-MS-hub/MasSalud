7e138390362404de02f982621c5b2fa3d3ae0447
Admin-MS-hub <administrador@massalud.org.pe>
2024-12-08 23:20:34 -0500

    CambioEstado

--- src/routes/RoutesDP.js
+++ src/routes/RoutesDP.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {deletePromocion, editPromocion, getPromociones, getPromocionesId, getTopPromociones, Image, postPromocion, RolUsuario, Rutas, UsuariosRol} from "../controller/DatosPControler.js";
+import {CambiarEstadoPR, CambiarEstados, deletePromocion, editPromocion, getEstadosUser, getFamiliares, getPromociones, getPromocionesId, getTopPromociones, Image, postFamiliares, postPromocion, RolUsuario, Rutas, UsuariosRol} from "../controller/DatosPControler.js";
 import { upload, verificarToken } from "../controller/UserController.js";
 
 const routerDP = Router();
@@ -29,5 +29,15 @@ routerDP.get('/Rutas/:id',verificarToken,Rutas)
 routerDP.get('/UsersRol/:id',verificarToken,UsuariosRol)
 
 routerDP.get('/RolUsuario/:id',verificarToken,RolUsuario)
+
+routerDP.post('/createFam',verificarToken,postFamiliares)
+
+routerDP.get('/GetFamiliares/:id',verificarToken,getFamiliares)
+
+routerDP.get('/EstadosUser',verificarToken,getEstadosUser)
+
+routerDP.put('/CambioEstado/:id',verificarToken,CambiarEstados)
+
+routerDP.put('/CambioEstadoPr/:id',verificarToken,CambiarEstadoPR)
 
 export default routerDP;
