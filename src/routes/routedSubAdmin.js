9b73bce68a2e42d5f806fab2e09a8051df0fb584
Admin-MS-hub <administrador@massalud.org.pe>
2024-12-10 13:00:12 -0500

    routedSubAdmin

--- src/routes/routedSubAdmin.js
+++ src/routes/routedSubAdmin.js
@@ -1,14 +1,16 @@
 import { Router } from "express";
-import { Afiliador, GenCode, GetPaginaHome, GetSubAdmin, GetSubAdministrador, Password } from "../controller/SubAdminController.js";
+import { Afiliador, CambiarEstadosFam, GenCode, GetFamiliares, getGananciaTotalGeneral, GetPaginaHome, GetSubAdmin, GetSubAdministrador, Password } from "../controller/SubAdminController.js";
 import { verificarToken } from "../controller/UserController.js";
+
 
 const SubRoutes=Router();
 
 SubRoutes.get('/GetSubAdmin/:id',verificarToken,GetSubAdmin);
 SubRoutes.get('/GetSubAdministrador/:id',verificarToken,GetSubAdministrador);
-SubRoutes.get('/GetPagHome',GetPaginaHome)
+SubRoutes.get('/GetPagHome',verificarToken,GetPaginaHome)
 SubRoutes.put('/Afiliador/:id',verificarToken,Afiliador)
-SubRoutes.put('/NewPasword/:id',Password)
+SubRoutes.put('/NewPasword/:id',verificarToken,Password)
 SubRoutes.post('/CodeGenered/:id',verificarToken,GenCode)
-
-export default SubRoutes;
+SubRoutes.get('/GanaciaTotal/:id',verificarToken,getGananciaTotalGeneral)
+SubRoutes.get('/GetFamiliaresGeneral',verificarToken,GetFamiliares)
+SubRoutes.put('/CambioEstadoFam/:id',verificarToken,CambiarEstadosFam)
