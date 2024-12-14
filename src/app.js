59b12db2b0daadd798d8e51a57d0e8452bcf5e4e
Admin-MS-hub <administrador@massalud.org.pe>
2024-12-14 11:27:41 -0500

    massalud

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 dotenv.config();
 const corsOptions = {
-    origin: ['https://darkgrey-ostrich-727682.hostingersite.com', 'http://localhost:3000','https://dniruc.apisperu.com'], // Orígenes permitidos
+    origin: ['https://massalud.org.pe', 'http://localhost:3000','https://dniruc.apisperu.com'], // Orígenes permitidos
     methods: ['OPTIONS', 'GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
     credentials: true, // Permite enviar cookies y encabezados de autorización
 };
