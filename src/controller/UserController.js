14410898845f12f4f35056c6a057d84871a0002e
Admin-MS-hub <administrador@massalud.org.pe>
2025-01-04 15:12:30 -0500

    sameSite

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -497,16 +497,19 @@ export const loginUsuario = async (req, res) => {
         res.cookie('refreshToken', refreshToken, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'None',
-            maxAge: 5 * 60 * 1000
+            maxAge: 5 * 60 * 1000,
+            sameSite: 'Strict',
+            secure: true, // Requiere HTTPS
+            domain: '.massalud.org.pe',
         });
 
         // Enviar el Access Token en una cookie HttpOnly
         res.cookie('accessToken', accessToken, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production', // Solo en producción, usar https
-            sameSite: 'None',
-            maxAge: 60 * 1000 // 1 minuto
+            secure: true, // Solo en producción, usar https
+            sameSite: 'Strict',
+            maxAge:5 * 60 * 1000, // 1 minuto
+            domain: '.massalud.org.pe',
         });
 
         // Responder con éxito, incluyendo los datos del usuario, sus vistas y el access token generado
@@ -762,13 +765,16 @@ export const logoutUsuario= async (req, res) => {
         // Eliminar las cookies de acceso y refresco
         res.clearCookie('accessToken', {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'None'
+            secure: true,
+            sameSite: 'Strict',
+            domain: '.massalud.org.pe',
         });
+
         res.clearCookie('refreshToken', {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'None'
+            secure: true,
+            sameSite: 'Strict',
+            domain: '.massalud.org.pe',
         });
 
         // Responder con éxito
@@ -844,9 +850,10 @@ export const refreshToken = async (req, res) => {
 
         res.cookie('accessToken', newAccessToken, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production', // Solo en producción, usar https
-            sameSite: 'None',
-            maxAge: 60 * 1000, // 1 minuto
+            secure: true, // Solo en producción, usar https
+            sameSite: 'Strict',
+            maxAge: 5* 60 * 1000, // 1 minuto
+            domain: '.massalud.org.pe',
         });
 
         // Retornar un valor para indicar que el token fue renovado
