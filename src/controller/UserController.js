0658a0f0f04e241343db76fc8d35846e7207e4a1
Diego Guevara <dgst1704@gmail.com>
2025-02-17 14:56:53 -0500

    estadoSolicitudMe

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -870,6 +870,7 @@ export const me = async (req, res) => {
                 u.apellidos, 
                 u.fotoPerfil, 
                 u.clinica_id, 
+                u.estado_solicitud,
                 r.nombre AS rol,
                 v.id AS vistaId, 
                 v.nombre AS vistaNombre, 
